87acbfd53e9d41df828138f9bf1dffac0e5cf8af
Berk <bdehrioglu@yahoo.com>
2017-01-03 15:21:27 +0300

    Add existing files

--- client.cpp
+++ client.cpp
@@ -1,9 +1,4 @@
-//
-// Copyright (c) 2013-2016 Vinnie Falco (vinnie dot falco at gmail dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
+
 
 
 #include <boost/asio.hpp>
